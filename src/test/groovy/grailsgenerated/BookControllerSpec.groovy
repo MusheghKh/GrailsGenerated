ee84b9abd8b14d0683491acf841ee12a46c60dac
Mushegh Khachatryan <mushegh.khachatryan@digitecintl.com>
2021-05-25 14:04:37 +0400

    BookControllerSpec

--- src/test/groovy/grailsgenerated/BookControllerSpec.groovy
+++ src/test/groovy/grailsgenerated/BookControllerSpec.groovy
@@ -20,9 +20,7 @@ class BookControllerSpec extends Specification implements ControllerUnitTest<Boo
     def populateValidParams(params) {
         assert params != null
 
-        // TODO: Populate valid properties like...
-        //params["name"] = 'someValidName'
-        assert false, "TODO: Provide a populateValidParams() implementation for this generated test suite"
+        params["name"] = 'someValidName'
     }
 
     void "Test the index action returns the correct response"() {
